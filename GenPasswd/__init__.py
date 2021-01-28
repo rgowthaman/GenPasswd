2eb452d609e19b7ba175cb51f998d183d8685b95
GOWTHAMAN R <61967645+Gowthaman1401@users.noreply.github.com>
2021-01-28 16:40:41 +0530

    Delete GenPasswd directory

--- GenPasswd/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Generate a strong password which includes alphabets, numbers, special characters"""
-
-__version__ = '1.1.2'
-
-from .genpasswd import Password
-from .functions import NoMultipleChoice, wanted_characters, include_characters, unwanted_characters, main
