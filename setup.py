ca7c5e787f5ef053c0bf018ea4a18dede727cd28
GOWTHAMAN R <61967645+Gowthaman1401@users.noreply.github.com>
2021-02-09 18:50:50 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='genpasswd',
     packages=setuptools.find_packages(),
-    version='1.1.5',
+    version='1.1.6',
     license='MIT',
     description='To generate random and strong passwords.',
     author='Gowthaman',
     author_email='rgngowthaman1@gmail.com',
     url='https://github.com/Gowthaman1401/GenPasswd',
-    download_url='https://github.com/Gowthaman1401/GenPasswd/archive/v1.1.5.tar.gz',
+    download_url='https://github.com/Gowthaman1401/GenPasswd/archive/v1.1.6.tar.gz',
     include_package_data=True,
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -25,7 +25,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entry_points={'console_scripts': ['password=genpasswd.__main__:main']
+    entry_points={'console_scripts': ['genpasswd=genpasswd.__main__:main']
                   },
     python_requires='>=3.6',
 )
