6507c38c5e6664a99781284a6a6f2c024d2ae446
GOWTHAMAN R <61967645+Gowthaman1401@users.noreply.github.com>
2021-02-09 15:43:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entry_points={'console_scripts': ['genpasswd=genpasswd.passgen:main']
+    entry_points={'console_scripts': ['genpasswd=genpasswd.__main__:main']
                   },
     python_requires='>=3.6',
 )
