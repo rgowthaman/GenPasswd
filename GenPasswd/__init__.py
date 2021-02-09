f633c1986a8b7e3693e576e6b032112c75045711
GOWTHAMAN R <61967645+Gowthaman1401@users.noreply.github.com>
2021-02-09 15:34:27 +0530

    Update __init__.py

--- GenPasswd/__init__.py
+++ GenPasswd/__init__.py
@@ -1,7 +1,7 @@
 """Generate a strong password which includes alphabets, numbers, special characters"""
 
-__version__ = '1.1.3'
+__version__ = '1.1.4'
 
 from .genpasswd import Password
 from .functions import NoMultipleChoice, wanted_characters, include_characters, unwanted_characters, main
-from .passgen import get_argument, gen_Pass, main
+from .__main__ import get_argument, gen_Password, main
