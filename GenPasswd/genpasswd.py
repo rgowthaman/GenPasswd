16c0ff284483363353013427af7f76e3d23961e3
GOWTHAMAN R <61967645+Gowthaman1401@users.noreply.github.com>
2021-03-31 14:41:49 +0530

    Update genpasswd.py

--- GenPasswd/genpasswd.py
+++ GenPasswd/genpasswd.py
@@ -2,12 +2,14 @@ from . import functions
 
 
 class Password:
-    def __init__(self, length=False, ignore=False, only=False, include=False, repeat=False):
+    def __init__(self, length=False, ignore=False, only=False, include=False, repeat=False, separator=False, separator_length=False, separation=False):
         self.length = length
         self.ignore = ignore
         self.only = only
         self.include = include
         self.repeat = repeat
+        self.separator_length = separator_length
+        self.separator = separator if separator else separation
 
     def generate(self):
-        return functions.main(Length=self.length, Unwanted=self.ignore, Only_char=self.only, Include=self.include, Repeat=self.repeat)
+        return functions.main(Length=self.length, Unwanted=self.ignore, Only_char=self.only, Include=self.include, Repeat=self.repeat, Seperator_length=self.separator_length, Seperator=self.separator)
